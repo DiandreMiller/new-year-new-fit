b157682e30fc42d84330beaa139300cc55427d74
Diandre Miller <millerdiandre@gmail.com>
2023-05-22 04:21:16 -0400

    env update

--- src/Components/firebase.jsx
+++ src/Components/firebase.jsx
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_WzXfx5LkOmuKGmZ5yF45CGU6leFoWQ8",
-  authDomain: "new-year-new-fit-c78c4.firebaseapp.com",
-  projectId: "new-year-new-fit-c78c4",
-  storageBucket: "new-year-new-fit-c78c4.appspot.com",
-  messagingSenderId: "575255318402",
-  appId: "1:575255318402:web:414a528553530fb579499b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
